2e69d7f91e49ffe3d166d52ba01cf89ef859ebff
Dominik Brych <dominik.brych@thenetw.org>
2026-06-24 12:33:17 +0200

    feat(Gantt): enhance SettingsSlider with theme support and styling

--- src/components/TaskGrid/components/header/settings-slider/SettingsSlider.tsx
+++ src/components/TaskGrid/components/header/settings-slider/SettingsSlider.tsx
@@ -1,23 +1,29 @@
-import { Slider } from "@fluentui/react";
+import { Slider, useTheme } from "@fluentui/react";
 import { useDatasetControl } from "../../../context";
 import { useEventEmitter } from "../../../../../hooks";
 import { IGanttGridBridgeEvents } from "../../../bridges";
 import { useRerender } from "@talxis/react-components";
+import { getSettingsSliderStyles } from "./styles";
+import { useMemo } from "react";
 
 export const SettingsSlider = () => {
     const datasetControl = useDatasetControl();
     const value = datasetControl.ganttGridBridge.getZoomLevel();
+    const theme = useTheme();
+    const styles = useMemo(() => getSettingsSliderStyles(theme), [theme]);
     const rerender = useRerender();
     useEventEmitter<IGanttGridBridgeEvents>(datasetControl.ganttGridBridge, 'onZoomLevelChanged', rerender);
-    
+
     return (
-        <Slider
-            min={0}
-            max={100}
-            value={value}
-            showValue
-            onChange={(nextValue) => {
-                datasetControl.ganttGridBridge.setZoomLevel(nextValue);
-            }} />
+        <div className={styles.root}>
+            <Slider
+                min={0}
+                max={100}
+                value={value}
+                showValue
+                onChange={(nextValue) => {
+                    datasetControl.ganttGridBridge.setZoomLevel(nextValue);
+                }} />
+        </div>
     );
 }
