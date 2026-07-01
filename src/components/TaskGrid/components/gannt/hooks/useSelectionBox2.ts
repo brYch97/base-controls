731725eb1fe2575275fd9794964aac3799021fa4
Dominik Brych <dominik.brych@thenetw.org>
2026-07-01 10:49:10 +0200

    feat(Gantt): enhance selection box with improved drag handling and selection state management

--- src/components/TaskGrid/components/gannt/hooks/useSelectionBox2.ts
+++ src/components/TaskGrid/components/gannt/hooks/useSelectionBox2.ts
@@ -1,10 +1,12 @@
-import Selecto, { OnDrag, OnDragStart, OnScroll, OnSelect } from "selecto";
+import Selecto, { OnDrag, OnDragEnd, OnDragStart, OnScroll, OnSelect } from "selecto";
 import { IGanttManager } from "../GanttManager";
 import { useEventEmitter } from "../../../../../hooks";
 import { useCallback, useEffect, useRef } from "react";
+import { useTaskDataProvider } from "../../../context";
 
-export const GANTT_TASK_LINK_CLASS = 'gantt_task_link';
+export const GANTT_TASK_LINE_CLASS = 'gantt_task_line';
 export const GANTT_SHIFT_HELD_CLASS = 'gantt_shift_held';
+export const GANT_SELECTED_CLASS = 'gantt_task_selected';
 
 const getDirection = (arr: number[]): 'up' | 'down' | 'left' | 'right' | null => {
     if (arr[0] === -1 && arr[1] === 0) return 'left';
@@ -17,26 +19,28 @@ const getDirection = (arr: number[]): 'up' | 'down' | 'left' | 'right' | null =>
 export const useSelectionBox = (ganttManager: IGanttManager) => {
     const gantt = ganttManager.getGanttInstance();
     const selectoRef = useRef<Selecto>();
+    const selectedIdsRef = useRef<string[]>([]);
+    const dataProvider = useTaskDataProvider();
     const lastScrollDirectionRef = useRef<'up' | 'down' | 'left' | 'right' | null>(null);
 
     const onInit = () => {
         const container = gantt.$task;
         selectoRef.current = new Selecto({
             container: container,
-            hitRate: 100,
-            toggleContinueSelect: ['shift'],
+            hitRate: 0,
             ratio: 0,
-            selectableTargets: [`.${GANTT_TASK_LINK_CLASS}`],
+            selectableTargets: [`.${GANTT_TASK_LINE_CLASS}`],
             scrollOptions: {
                 container: gantt.$scroll_hor,
                 throttleTime: 30,
-                threshold: 100,
+                //threshold: 100,
             }
 
         });
         selectoRef.current.on('select', onSelect);
         selectoRef.current.on('scroll', onScroll);
         selectoRef.current.on('dragStart', onDragStart);
+        selectoRef.current.on('dragEnd', onDragEnd);
         selectoRef.current.on('drag', onDrag);
         window.addEventListener('keyup', onKeyUp);
         window.addEventListener('keydown', onKeyDown);
@@ -55,24 +59,39 @@ export const useSelectionBox = (ganttManager: IGanttManager) => {
     }, []);
 
     const onSelect = (e: OnSelect<Selecto>) => {
+        e.removed.forEach(el => {
+            el.classList.remove(GANT_SELECTED_CLASS);
+        })
         e.added.forEach(el => {
-            console.log('Selected element:', el);
+            el.classList.add(GANT_SELECTED_CLASS);
         });
+        selectedIdsRef.current = e.selected.map(el => el.getAttribute('data-task-id')!);
     };
 
     const onDragStart = (e: OnDragStart<Selecto>) => {
         if (!e.inputEvent.shiftKey) {
             e.stop();
         }
+        onDrag(e as any);
     };
 
     const onDrag = (e: OnDrag<Selecto>) => {
-        const absDeltax = Math.abs(e.deltaX);
-        const absDeltay = Math.abs(e.deltaY);
-        if (absDeltax > absDeltay) {
+        const { clientX, clientY } = e.inputEvent as MouseEvent;
+        const rect = (gantt.$task as HTMLElement).getBoundingClientRect();
+
+        const distToHorizontalEdge = Math.min(Math.abs(clientX - rect.left), Math.abs(clientX - rect.right));
+        const distToVerticalEdge = Math.min(Math.abs(clientY - rect.top), Math.abs(clientY - rect.bottom));
+
+        if (distToHorizontalEdge < distToVerticalEdge) {
             selectoRef.current!.scrollOptions.container = gantt.$scroll_hor;
-        } else if (absDeltay > absDeltax) {
+        } else {
             selectoRef.current!.scrollOptions.container = gantt.$scroll_ver;
+        }
+    }
+
+    const onDragEnd = (e: OnDragEnd<Selecto>) => {
+        if(selectedIdsRef.current.length > 0) {
+            dataProvider.setSelectedRecordIds(selectedIdsRef.current);
         }
     }
 
